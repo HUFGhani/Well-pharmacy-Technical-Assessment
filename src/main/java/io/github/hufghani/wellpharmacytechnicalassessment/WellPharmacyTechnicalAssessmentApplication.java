5781e371b7ca99a459c162427be4bdb8e0de7f53
Hamza Umar Farooq Ghani <hamzaumarfarooqghani@rentalcars.com>
2020-08-25 14:50:40 +0100

    first commit

---
+++ src/main/java/io/github/hufghani/wellpharmacytechnicalassessment/WellPharmacyTechnicalAssessmentApplication.java
@@ -0,0 +1,12 @@
+package io.github.hufghani.wellpharmacytechnicalassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellPharmacyTechnicalAssessmentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WellPharmacyTechnicalAssessmentApplication.class, args);
+  }
+}
---
+++ src/test/java/io/github/hufghani/wellpharmacytechnicalassessment/WellPharmacyTechnicalAssessmentApplicationTests.java
@@ -0,0 +1,11 @@
+package io.github.hufghani.wellpharmacytechnicalassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellPharmacyTechnicalAssessmentApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
